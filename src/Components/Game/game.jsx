234c56e601312025926d8e2447cfdcfea3319235
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-03 11:58:04 -0300

    cambio en game

--- src/Components/Game/game.jsx
+++ src/Components/Game/game.jsx
@@ -3,11 +3,30 @@ const Game = () => {
   const $paragraph = document.querySelector("p");
   const $input = document.querySelector("input");
 
+  const INITIAL_TIME = 30;
+
+  const TEXT = "They will have the following structure: first page same as the original articles, summary in Castilian and English; then, the introduction should be a short paragraph that gives some message about the case."
+
+  let words = [];
+  let currentTime = INITIAL_TIME;
+
+  initGame()
+  initEvents()
+
+  function initGame() {
+    words = TEXT.split(" ").slice(0, 32);
+    currentTime = INITIAL_TIME;
+
+    $time.textContent = currentTime;
+    
+  } 
+  function initEvents() { }
+  
 
   return (
     <div className="Game">
       <body>
-        <time>30</time>
+        <time></time>
         <p></p>
         <input type="text" autoFocus />
       </body>
